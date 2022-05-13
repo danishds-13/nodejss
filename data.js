aa7311f61de11ffcae67e59e6344ffebe623cf1f
danishdyabala2519-85 <danishdeepasoman02@gmail.com>
2022-05-13 15:47:19 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,6 @@
+const data=[
+    {name:'danish',email:'danishdeepasoman02@gmail.com'},
+    {name:'anil',email:'anil02@gmail.com'},
+    {name:'dhanush',email:'ddh02@gmail.com'}
+]
+module.exports=data;
